04ad9a49168fc247516885e7b28dad4ee7e73784
needMoreGank <104450913+needMoreGank@users.noreply.github.com>
2022-09-30 18:27:32 +0900

    Update pxt.json, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-def on_on_overlap(sprite, otherSprite):
-    game.over(False)
-sprites.on_overlap(SpriteKind.player, SpriteKind.projectile, on_on_overlap)
-
-# tile size is 16*16
-tiles.set_current_tilemap(tilemap("""
-    level2
-"""))
-# HW:
-# Sprite design ART
-# barbed wire, mine, bullet, blood, random stuff
-# research trenches FOR REAL TO SEE IF IT WORKS
+tiles.set_current_tilemap(tilemap("""level1"""))
+# tile size is 1enum)s FOR REAL TO SEE IF IT WORKS
 # GITHUB AGAINST
+#enum = 
 hp_max = 100
-hp_current = hp_max
+hp = hp_max
 messenger = sprites.create(img("""
         . . . . . . f f f f . . . . . . 
             . . . . f f f 2 2 f f f . . . . 
@@ -35,3 +26,86 @@ messenger = sprites.create(img("""
 currentScene = "game"
 controller.move_sprite(messenger)
 scene.camera_follow_sprite(messenger)
+
+
+def spawn_leftBullet():
+    leftBullet = sprites.create(img("""
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        . 5 f 5 5 5 . .
+        . 5 f 5 5 5 5 .
+        . 5 f 5 5 5 . .
+        . . . . . . . .
+        . . . . . . . .
+    """), SpriteKind.projectile)
+    leftBullet.set_position(0, randint(16, 496))
+    leftBullet.set_velocity(randint(60, 130), 0)
+    leftBullet.start_effect(effects.trail)
+game.on_update_interval(randint(500, 1000), spawn_leftBullet)
+
+def spawn_rightBullet():
+    rightBullet = sprites.create(img("""
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        . . 5 5 5 f 5 .
+        . 5 5 5 5 f 5 .
+        . . 5 5 5 f 5 .
+        . . . . . . . .
+        . . . . . . . .
+    """), SpriteKind.projectile)
+    rightBullet.set_position(270, randint(16, 496))
+    rightBullet.set_velocity(randint(-60, -130), 0)
+    rightBullet.start_effect(effects.trail)
+    
+game.on_update_interval(randint(500, 1000), spawn_rightBullet)
+
+def on_hit_wall(sprite, location):
+    sprite.destroy()
+scene.on_hit_wall(SpriteKind.projectile, on_hit_wall)
+def hit_bullet(sprite, bullet):
+    global hp
+    hp = hp - 20
+    if hp == 0:
+        game.over(False)
+    bullet.destroy()
+
+sprites.on_overlap(SpriteKind.player, SpriteKind.projectile, hit_bullet)
+
+#barbed_wire
+wire = sprites.create(img("""
+    f f f f 1 f f f f 1 f f f f f f
+    f f f f 1 f f f f 1 f f 1 f f f
+    f f f f 1 f f f 1 f f f 1 f f f
+    f f f f 1 f f f 1 f f f 1 f f f
+    f f 1 1 f f f 1 1 f f 1 f f f f
+    f 1 f f f f f 1 f f f 1 f f f f
+    f f f f f f f f f f f 1 f f f f
+    f f f f f f f f f f f f f f f f
+"""),
+    SpriteKind.enemy)
+
+wire.set_position(30, 30)
+scaling.scale_to_pixels(wire, randint(32, 64), ScaleDirection.HORIZONTALLY, ScaleAnchor.MIDDLE)
+
+#landmine
+landmine = sprites.create(img("""
+    . . . . 6 6 6 6 6 6 6 . . . . .
+    . . 6 6 6 6 6 6 6 6 6 6 6 . . .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 .
+    6 6 6 6 6 6 6 f 6 6 2 6 6 6 6 .
+    6 6 6 6 6 6 f f f 6 6 6 6 6 6 .
+    6 6 6 6 6 f f f f f 6 6 6 6 6 .
+    6 6 6 6 6 6 f f f 6 6 6 6 6 6 .
+    6 6 6 6 6 6 6 f 6 6 6 6 6 6 6 .
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    . . 6 6 6 6 6 6 6 6 6 6 6 . . .
+    . . . . 6 6 6 6 6 6 6 . . . . .
+    . . . . . . . . . . . . . . . .
+"""),
+    SpriteKind.enemy)
--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
-    game.over(false)
-})
-//  tile size is 16*16
-tiles.setCurrentTilemap(tilemap`
-    level2
-`)
-//  HW:
-//  Sprite design ART
-//  barbed wire, mine, bullet, blood, random stuff
-//  research trenches FOR REAL TO SEE IF IT WORKS
+tiles.setCurrentTilemap(tilemap`level1`)
+//  tile size is 1enum)s FOR REAL TO SEE IF IT WORKS
 //  GITHUB AGAINST
+// enum = 
 let hp_max = 100
-let hp_current = hp_max
+let hp = hp_max
 let messenger = sprites.create(img`
         . . . . . . f f f f . . . . . . 
             . . . . f f f 2 2 f f f . . . . 
@@ -33,3 +25,77 @@ let messenger = sprites.create(img`
 let currentScene = "game"
 controller.moveSprite(messenger)
 scene.cameraFollowSprite(messenger)
+game.onUpdateInterval(randint(500, 1000), function spawn_leftBullet() {
+    let leftBullet = sprites.create(img`
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        . 5 f 5 5 5 . .
+        . 5 f 5 5 5 5 .
+        . 5 f 5 5 5 . .
+        . . . . . . . .
+        . . . . . . . .
+    `, SpriteKind.Projectile)
+    leftBullet.setPosition(0, randint(16, 496))
+    leftBullet.setVelocity(randint(60, 130), 0)
+    leftBullet.startEffect(effects.trail)
+})
+game.onUpdateInterval(randint(500, 1000), function spawn_rightBullet() {
+    let rightBullet = sprites.create(img`
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        . . 5 5 5 f 5 .
+        . 5 5 5 5 f 5 .
+        . . 5 5 5 f 5 .
+        . . . . . . . .
+        . . . . . . . .
+    `, SpriteKind.Projectile)
+    rightBullet.setPosition(270, randint(16, 496))
+    rightBullet.setVelocity(randint(-60, -130), 0)
+    rightBullet.startEffect(effects.trail)
+})
+scene.onHitWall(SpriteKind.Projectile, function on_hit_wall(sprite: Sprite, location: tiles.Location) {
+    sprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function hit_bullet(sprite: Sprite, bullet: Sprite) {
+    
+    hp = hp - 20
+    if (hp == 0) {
+        game.over(false)
+    }
+    
+    bullet.destroy()
+})
+// barbed_wire
+let wire = sprites.create(img`
+    f f f f 1 f f f f 1 f f f f f f
+    f f f f 1 f f f f 1 f f 1 f f f
+    f f f f 1 f f f 1 f f f 1 f f f
+    f f f f 1 f f f 1 f f f 1 f f f
+    f f 1 1 f f f 1 1 f f 1 f f f f
+    f 1 f f f f f 1 f f f 1 f f f f
+    f f f f f f f f f f f 1 f f f f
+    f f f f f f f f f f f f f f f f
+`, SpriteKind.Enemy)
+wire.setPosition(30, 30)
+scaling.scaleToPixels(wire, randint(32, 64), ScaleDirection.Horizontally, ScaleAnchor.Middle)
+// landmine
+let landmine = sprites.create(img`
+    . . . . 6 6 6 6 6 6 6 . . . . .
+    . . 6 6 6 6 6 6 6 6 6 6 6 . . .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 .
+    6 6 6 6 6 6 6 f 6 6 2 6 6 6 6 .
+    6 6 6 6 6 6 f f f 6 6 6 6 6 6 .
+    6 6 6 6 6 f f f f f 6 6 6 6 6 .
+    6 6 6 6 6 6 f f f 6 6 6 6 6 6 .
+    6 6 6 6 6 6 6 f 6 6 6 6 6 6 6 .
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    . 6 6 6 6 6 6 6 6 6 6 6 6 6 . .
+    . . 6 6 6 6 6 6 6 6 6 6 6 . . .
+    . . . . 6 6 6 6 6 6 6 . . . . .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Enemy)
